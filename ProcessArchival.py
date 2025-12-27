991c1021fce810ae9729b899684cac3dae1f07f4
WuBinBang <13705894451>
2025-12-27 10:24:29 +0800

    Update ProcessArchival

--- ProcessArchival.py
+++ ProcessArchival.py
@@ -1,11 +1,12 @@
 import os
 from datetime import datetime
-from colorama import Fore, Style
+import colorama as ca
 from tkinter import *
 
+ca.init()
 global PATH, OFFSETS, VERSION
 OFFSETS = 825783
-VERSION = '1.0.0'
+VERSION = 100
 PATH = "C:\\Users\\Administrator\\AppData\\LocalLow\\FarmGame\\"
 
 def string_shift(text, offset):
@@ -20,17 +21,20 @@ def CheckIfTheFolderExists():
         os.makedirs(directory)
 
 def VersionComparison():
-    root = Tk()
-    
-    VERSION_LIST = list(map(int, VERSION.split('.')))
     with os.scandir(PATH) as entries:
         for entry in entries:
-            Button(root, text = "ARCHIVAL", command = lambda : OpenFile(entry.name)).pack()
             print(entry.name, end = ' -> ')
             with open(PATH + entry.name, 'r', encoding='utf-8') as f:
                 lines = string_shift(f.readlines()[0], -OFFSETS).split('\n')[0].split('=')[1]
-                print('version:', lines)
-    root.mainloop()
+                Version = list(map(int, lines.split('.')))
+                print('version:', end = ' ')
+                if Version[0] * 100 + Version[1] * 10 + Version[2] == VERSION:
+                    print(ca.Fore.GREEN + (lines + ' 存档版本号等于游戏版本号, 可以游玩!') + ca.Fore.RESET)
+                elif Version[0] * 100 + Version[1] * 10 + Version[2] < VERSION:
+                    print(ca.Fore.YELLOW + (lines + ' 存档版本号低于游戏版本号, 如使用该存档启动游戏将自动覆盖') + ca.Fore.RESET)
+                else:
+                    print(ca.Fore.RED + (lines + ' 存档版本号高于游戏版本号, 请检查你的游戏版本是否是最新版本') + ca.Fore.RESET)
+                
 
 def SaveFile():
     with open(PATH + f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S').replace(' ', '_').replace(':', '')}.sav", 'w', encoding = 'UTF-8') as f:
@@ -46,3 +50,4 @@ CheckIfTheFolderExists()
 
 if __name__ == '__main__':
     VersionComparison()
+    #SaveFile()
